236ced2c3f62ba6d558b220e46876ac592232496
ishikawamitsugi <ishikawamitsugi@gmail.com>
2021-02-16 23:56:53 +0900

    BottomNavigationBarのバグを修正

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -18,11 +18,12 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Scaffold(
       body: _pages[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
         items: [
           BottomNavigationBarItem(
               label: 'トップニュース', icon: Icon(Icons.highlight)),
           BottomNavigationBarItem(label: 'ニュース一覧', icon: Icon(Icons.list)),
-          BottomNavigationBarItem(label: 'このアプリについて', icon: Icon(Icons.info))
+          BottomNavigationBarItem(label: 'このアプリについて', icon: Icon(Icons.info)),
         ],
         onTap: (index) {
           setState(() {
