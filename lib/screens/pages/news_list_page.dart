90b3289bf793c5142092385be946187a34156703
ishikawamitsugi <ishikawamitsugi@gmail.com>
2021-02-16 23:23:56 +0900

    FloatingActionButtonを表示した

--- lib/screens/pages/news_list_page.dart
+++ lib/screens/pages/news_list_page.dart
@@ -4,8 +4,31 @@ class NewsListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child:
-          Scaffold(body: Container(child: Center(child: Text('NewsListPage')))),
+      child: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.refresh),
+          tooltip: '更新',
+          onPressed: () => onRefresh(context)
+          ),
+        body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child:Column(
+              children: [
+                // TODO
+                // SerchBar(),
+                // CategoryChips(),
+                Expanded(
+                  child: Center(
+                    child: CircularProgressIndicator(),
+                  )
+                ),
+              ],
+            )
+        )),
     );
+  }
+
+  // TODO
+  onRefresh (BuildContext context) {
   }
 }
