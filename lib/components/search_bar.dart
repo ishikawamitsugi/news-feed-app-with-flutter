8237f037a53e12d9df82b6dc17f4903eba23748c
ishikawamitsugi <ishikawamitsugi@gmail.com>
2021-02-17 00:31:26 +0900

    検索をした際に関数を呼ぶようにした

--- lib/components/search_bar.dart
+++ lib/components/search_bar.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 
 class SearchBar extends StatelessWidget {
+
+  final ValueChanged onSearch;
+  final TextEditingController _textEditingController = TextEditingController();
+
+  SearchBar({this.onSearch});
+
   @override
   Widget build(BuildContext context) {
     return Card(
-        elevation: 2.0,
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.all(Radius.circular(24))),
-        child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24),
-            child: TextField(
-              maxLines: 1,
-              decoration: InputDecoration(
-                  icon: Icon(Icons.search),
-                  hintText: '検索ワードを入力してください',
-                  border: InputBorder.none),
-            )));
+      elevation: 2.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(24),
+        ),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: TextField(
+          controller: _textEditingController,
+          onSubmitted: this.onSearch,
+          maxLines: 1,
+          decoration: InputDecoration(
+            icon: Icon(Icons.search),
+            hintText: '検索ワードを入力してください',
+            border: InputBorder.none,
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/view/screens/pages/news_list_page.dart
+++ lib/view/screens/pages/news_list_page.dart
@@ -6,26 +6,34 @@ class NewsListPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-          floatingActionButton: FloatingActionButton(
-              child: Icon(Icons.refresh),
-              tooltip: '更新',
-              onPressed: () => onRefresh(context)),
-          body: Padding(
-              padding: EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  // TODO
-                  SearchBar(),
-                  // CategoryChips(),
-                  Expanded(
-                      child: Center(
-                    child: CircularProgressIndicator(),
-                  )),
-                ],
-              ))),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.refresh),
+          tooltip: '更新',
+          onPressed: () => onRefresh(context),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SearchBar(
+                onSearch: (keyword) => getKeywordNews(context, keyword),
+              ),
+              // CategoryChips(),
+              Expanded(
+                  child: Center(
+                child: CircularProgressIndicator(),
+              )),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
   // TODO
   onRefresh(BuildContext context) {}
+
+  getKeywordNews(BuildContext context, String keyword) {
+    print("my input keyword: ${keyword}");
+  }
 }
