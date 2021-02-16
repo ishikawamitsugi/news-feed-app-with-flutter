88f45d32aba2d99005a7e465c212e49a2943867b
ishikawamitsugi <ishikawamitsugi@gmail.com>
2021-02-16 23:52:43 +0900

    検索ワード入力欄を作成した

---
+++ lib/components/search_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 2.0,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(24))),
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: TextField(
+              maxLines: 1,
+              decoration: InputDecoration(
+                  icon: Icon(Icons.search),
+                  hintText: '検索ワードを入力してください',
+                  border: InputBorder.none),
+            )));
+  }
+}
