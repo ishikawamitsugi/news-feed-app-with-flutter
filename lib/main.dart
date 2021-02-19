541456375ec72c7d66e2468196c533b9fe6ba0ca
ishikawamitsugi <ishikawamitsugi@gmail.com>
2021-02-19 22:59:33 +0900

    dotEnvを導入してAPIKEYを隠蔽した

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:news_feed/view/screens/home_screen.dart';
 import 'package:news_feed/view/style/style.dart';
 import 'package:news_feed/view_models/news_list_view_model.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
-void main() {
+void main() async {
+  await DotEnv.load(fileName: ".env");
   return runApp(ChangeNotifierProvider(
     create: (context) => NewsListViewModel(),
     lazy:false, // 遅延読み込みをやめる
---
+++ lib/models/networking/api_service.dart
@@ -0,0 +1,45 @@
+import 'package:chopper/chopper.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+
+part 'api_service.chopper.dart';
+
+// API_KEYは下記のような記述を行う。
+// static final API_KEY = DotEnv.env["NEWS_API_KEY"];
+
+@ChopperApi()
+abstract class ApiService extends ChopperService {
+  static const BASE_URL = "http://newsapi.org/v2/";
+  static final API_KEY = DotEnv.env["NEWS_API_KEY"];
+
+  static create() {
+    final client = ChopperClient(
+      baseUrl: BASE_URL,
+      services: [
+        _$ApiService()
+      ],
+      converter: JsonConverter(),
+      errorConverter: JsonConverter(),
+    );
+    return client.getService<ApiService>();
+  }
+
+  @Get(path: "/top-headlines")
+  Future<Response> getHeadLines(
+      {@Query("country") String country = "jp",
+      @Query("pageSize") int pageSize = 10,
+      @Query("apiKey") String apiKey});
+
+  @Get(path: "/top-headlines")
+  Future<Response> getKeywordNews(
+      {@Query("country") String country = "jp",
+      @Query("pageSize") int pageSize = 30,
+      @Query("q") String keyword,
+      @Query("apiKey") String apiKey});
+
+  @Get(path: "/top-headlines")
+  Future<Response> getCategoryNews(
+      {@Query("country") String country = "jp",
+      @Query("pageSize") int pageSize = 30,
+      @Query("category") String category,
+      @Query("apiKey") String apiKey});
+}
