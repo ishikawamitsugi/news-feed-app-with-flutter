28817c783b1361b861a83566857d806e5f56dac0
ishikawamitsugi <ishikawamitsugi@gmail.com>
2021-02-22 21:50:38 +0900

    MultiProviderを作成して複数のViewModelをWigetツリーの上部に登録するようにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:news_feed/view/screens/home_screen.dart';
 import 'package:news_feed/view/style/style.dart';
+import 'package:news_feed/view_models/head_line_view_model.dart';
 import 'package:news_feed/view_models/news_list_view_model.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 
 void main() async {
   await DotEnv.load(fileName: ".env");
-  return runApp(ChangeNotifierProvider(
-    create: (context) => NewsListViewModel(),
-    lazy:false, // 遅延読み込みをやめる
+  return runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(
+        create: (_) => NewsListViewModel(),
+      ),
+      ChangeNotifierProvider(
+        create: (_) => HeadLineViewModel(),
+      ),
+    ],
     child: MyApp(),
   ));
 }
