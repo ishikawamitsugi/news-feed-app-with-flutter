62a720cc5b2a070ebb7cd525a1e6ac9631589f81
ishikawamitsugi <ishikawamitsugi@gmail.com>
2021-02-22 22:11:18 +0900

    HeadLineページに記事の内容を表示するような雛形を作成した

--- lib/view/screens/pages/head_line_page.dart
+++ lib/view/screens/pages/head_line_page.dart
@@ -9,7 +9,7 @@ class HeadLinePage extends StatelessWidget {
     Future(() {
       // 値の通知はうけるがリビルドはしない
       final viewModel = context.read<HeadLineViewModel>();
-      if(!viewModel.isLoading && viewModel.articles.isEmpty) {
+      if (!viewModel.isLoading && viewModel.articles.isEmpty) {
         // Articlesを取得してViewModelの中に格納する
         viewModel.getHeadLineNews(searchType: SearchType.HEAD_LINE);
       }
@@ -17,10 +17,22 @@ class HeadLinePage extends StatelessWidget {
 
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          child: Center(
-            child:  Text('HeadLineNews')
-          ),
+        body: Consumer<HeadLineViewModel>(
+          builder: (context, viewModel, child) {
+            return PageView.builder(
+              controller: PageController(initialPage: 0),
+              scrollDirection: Axis.horizontal,
+              itemCount: viewModel.articles.length,
+              itemBuilder: (context, index) {
+                final article = viewModel.articles[index];
+                return Container(
+                  child: Column(
+                    children: [Text(article.title), Text(article.description)],
+                  ),
+                );
+              },
+            );
+          },
         ),
         floatingActionButton: FloatingActionButton(
           child: Icon(Icons.refresh),
@@ -31,7 +43,7 @@ class HeadLinePage extends StatelessWidget {
   }
 
   onRefresh(BuildContext context) async {
-    print ("HeadLinePage.onRefresh");
+    print("HeadLinePage.onRefresh");
     final viewModel = context.read<HeadLineViewModel>();
     await viewModel.getHeadLineNews(searchType: SearchType.HEAD_LINE);
   }
